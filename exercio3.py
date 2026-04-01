62ab40564e49b0a2721865f3ac45dbf822236dbc
Landooliveira <leandro.oliveirasant@yahoo.com>
2026-04-01 20:04:18 -0300

    Create exercio3.py

---
+++ exercio3.py
@@ -0,0 +1,9 @@
+# Escreva um algoritimo para perguntar as 4 notas de uma aluno e informar a media
+print ("Digite a base")
+base = int(input())
+print ("Digite a altura")
+altura = int(input())
+area = base * altura
+
+
+print  (area)
