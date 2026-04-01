bcdae088dceca415d9c90945ad59721a5dd2aa55
Landooliveira <leandro.oliveirasant@yahoo.com>
2026-04-01 19:44:26 -0300

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,13 @@
+# Escreva um algoritimo para perguntar as 4 notas de uma aluno e informar a media
+print ("Digite a nota")
+nota1 = int(input())
+print ("Digite a segunda nota")
+nota2 = int(input())
+print ("Digite a terceira nota")
+nota3 = int(input())
+print ("Digite a quarta nota")
+nota4 = int(input()) 
+
+resultado = nota1 + nota2 + nota3 + nota4 
+media = resultado / 4
+print (media)
