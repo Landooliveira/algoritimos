66b6619812813f409cafc23e47450078244d9e01
Landooliveira <leandro.oliveirasant@yahoo.com>
2026-04-08 20:20:20 -0300

    Create exercicio5.py

---
+++ exercicio5.py
@@ -0,0 +1,12 @@
+quantidade = int(input("Digite o numero de macas compradas:"))
+
+if quantidade < 12: 
+    preco =1.30
+    
+
+else:
+   preco = 1.00
+   
+total = quantidade * preco
+   
+print(total)
